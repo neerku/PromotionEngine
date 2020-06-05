176f69328d8c4227bfbf93b7fbeec19f091b8545
Neeraj Kumar <neeraj.kumar@zf.com>
2020-06-05 15:18:39 +0530

    Added Initial Solution Structure

---
+++ PromotionEngine/PromotionEngineTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PromotionEngineTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PromotionEngineTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eb289255-296b-4368-a023-c2f8c599a10e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PromotionEngine/PromotionEngineTest/TestEngine.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngineTest
+{
+    [TestClass]
+    internal class TestEngine
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+        }
+
+        [TestMethod]
+        public void TestScenarioA()
+        {
+        }
+
+        [TestMethod]
+        public void TestScenarioB()
+        {
+        }
+
+        [TestMethod]
+        public void TestScenarioC()
+        {
+        }
+    }
+}
