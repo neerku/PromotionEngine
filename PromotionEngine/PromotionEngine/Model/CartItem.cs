187f596d9e877f033e60de405d843414f44acaf8
Neeraj Kumar <neeraj.kumar@zf.com>
2020-06-05 15:33:03 +0530

    Added Model.

---
+++ PromotionEngine/PromotionEngine/Model/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class CartItem
+    {
+        public SKUItem Item { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ PromotionEngine/PromotionEngine/Model/SKUItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class SKUItem
+    {
+        public string Id { get; set; }
+
+        public int Price { get; set; }
+
+        public int PromotionalId { get; set; }
+    }
+}
